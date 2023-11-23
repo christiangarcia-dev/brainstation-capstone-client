fae9880647ff688af4202b55c983d413caa11c3d
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-23 10:56:33 -0500

    firebase auth and db linked to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Auth } from './components/auth';
+import { db, auth, storage } from './config/firebase';
+import { useEffect } from 'react';
+import { getDocs, getDoc, collection, deleteDoc, updateDoc, doc } from 'firebase/firestore';
+import { ref, uploadBytes } from "firebase/storage";
+import { async } from '@firebase/util';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+
+  const [transcripts, setTranscripts] = useState([]);
+
+  // states for saving a new conversation 
+
+  const transcriptsCollectionRef = collection(db, "transcripts");
+
+  useEffect(() => {
+    const getTranscripts = async () => {
+      // READ THE DATA FROM THE DB 
+      // SET THE TRANSCRIPT CONTENT 
+      try {
+        const data = await getDocs(transcriptsCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setTranscripts(filteredData);
+        console.log(filteredData)
+      } catch (error) {
+          console.error(error);
+      }
+    }
+    getTranscripts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auth />
+
+      <div>
+        {/* for this app, inputs would be user who is logged in and translations from a given seession
+        IF saved */}
+        <input></input>
+        <input></input>
+      </div>
+
+      <div>
+        {transcripts.map((transcript) => (
+          <div>
+            <h1>{transcript.fromUser}</h1>
+            <p>{transcript.conversation}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBWiQFLOWkJfMYdF3e-DGL1QC16NqnJPjo",
+    authDomain: "echolingo-dd50f.firebaseapp.com",
+    projectId: "echolingo-dd50f",
+    storageBucket: "echolingo-dd50f.appspot.com",
+    messagingSenderId: "824689091852",
+    appId: "1:824689091852:web:f0d5be1b3732535e7a5529",
+    measurementId: "G-KDC4E6HKNT"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
