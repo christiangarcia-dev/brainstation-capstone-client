cafad95330b8297077a0bd54436590e1db6db7f7
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-26 09:52:43 -0500

    moving over to styling for now

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,12 @@
 import "./LoginPage.scss";
 import { Auth } from "../../components/auth/auth";
+import Header from "../../components/Header/Header";
 
 function LoginPage() {
 
     return(
         <>
+            <Header />
             <Auth />
         </>
     )
