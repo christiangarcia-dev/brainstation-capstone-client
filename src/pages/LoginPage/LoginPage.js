7e50018e18de1b28b0fd0eadd497d992a6f62142
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-26 15:18:57 -0500

    sign up styling and refactoring complete (only need animations)

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,18 +1,18 @@
 import "./LoginPage.scss";
 import LoginForm from "../../components/LoginForm/LoginForm";
-import Logo from "../../assets/logo/echolingo-logo.png";
+import Logo from "../../assets/logo/echolingo-logo.svg";
 import { Link } from "react-router-dom";
 
 function LoginPage() {
 
     return (
         <main className="login-page" alt="login page">
-            <div className="login-page__logo-container">
+            <div className="login-page__logo--container">
                 <img className="login-page__logo" src={Logo} alt="echolingo logo"></img>
             </div>
             <LoginForm />
             <p className="login-page__or">- OR -</p>
-            <p className="login-page__cta">Create an account? <Link className="login-page__cta-signup" to="/signup">Sign-Up</Link></p>
+            <p className="login-page__cta">Create an account? <Link className="login-page__cta--signup" to="/signup">Sign-Up</Link></p>
         </main>
     );
 }
