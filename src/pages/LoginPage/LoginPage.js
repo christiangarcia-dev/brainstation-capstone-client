5885424dd04d1fd07783912be371d1d0ef81ac9a
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-26 20:16:12 -0500

    login page fully styled and responsive

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -7,12 +7,20 @@ function LoginPage() {
 
     return (
         <main className="login-page" alt="login page">
-            <div className="login-page__logo--container">
-                <img className="login-page__logo" src={Logo} alt="echolingo logo"></img>
-            </div>
-            <LoginForm />
-            <p className="login-page__or">- OR -</p>
-            <p className="login-page__cta">Create an account? <Link className="login-page__cta--signup" to="/signup">Sign-Up</Link></p>
+            <section className="login-page__header--container">
+                <section className="login-page__header">
+                    <div className="login-page__header--box"></div>
+                        <div className="login-page__logo--container">
+                            <img className="login-page__logo" src={Logo} alt="echolingo logo"></img>
+                        </div>
+                    <div className="login-page__header--box"></div>
+                </section>
+            </section>
+            <section className="login-page__form--container">
+                <LoginForm />
+                <p className="login-page__or">- OR -</p>
+                <p className="login-page__cta">Create an account? <Link className="login-page__cta--signup" to="/signup">Sign-Up</Link></p>
+            </section>
         </main>
     );
 }
