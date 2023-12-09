bb9d47ef85c0f441b59a90e9783718f7664c8bbe
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-12-08 19:16:33 -0500

    new upload modal compelte with more streamlined functionality

--- src/components/fileUploadForm/fileUploadForm.js
+++ src/components/fileUploadForm/fileUploadForm.js
@@ -1,43 +1,42 @@
-import "./FileUploadForm.scss";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import { auth, db } from '../../config/firebase';
-import { collection, addDoc } from 'firebase/firestore';
-import { doc, getDoc } from 'firebase/firestore';
-import { Link } from "react-router-dom";
-import axios from 'axios';
-import { useState, useEffect } from "react";
+import { collection, addDoc, doc, getDoc } from "firebase/firestore";
 import closeModalIcon from "../../assets/icons/close-black.svg";
 import uploadCloudIcon from "../../assets/icons/upload.svg";
 import audioFileIcon from "../../assets/icons/audio-file.svg";
+import "./FileUploadForm.scss";
 
 function FileUploadForm() {
-
     const [selectedFile, setSelectedFile] = useState(null);
     const [transcription, setTranscription] = useState('');
     const [isModalOpen, setIsModalOpen] = useState(true);
+    const hiddenFileInput = useRef(null);
 
     useEffect(() => {
         if (transcription) setIsModalOpen(false);
     }, [transcription]);
 
-    const handleFileChange = (event) => {
-        setSelectedFile(event.target.files[0]);
+    const handleClick = () => {
+        hiddenFileInput.current.click();
     };
 
-    const openModal = () => {
-        setIsModalOpen(true);
+    const handleFileChange = async (event) => {
+        const file = event.target.files[0];
+        setSelectedFile(file);
+        await handleSubmit(file);
     };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setTranscription(''); 
+    const handleSubmit = async (file) => {
+        setTranscription('');
 
-        if (!selectedFile) {
+        if (!file) {
             alert('Please select a file to upload');
             return;
         }
 
         const formData = new FormData();
-        formData.append('file', selectedFile);
+        formData.append('file', file);
 
         try {
             const response = await axios.post('http://localhost:8080/api/whisper/transcribe', formData, {
@@ -45,7 +44,6 @@ function FileUploadForm() {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-            console.log('Transcription:', response.data);
             setTranscription(response.data.text || 'Transcription not available');
         } catch (error) {
             console.error('Error uploading file:', error);
@@ -53,90 +51,77 @@ function FileUploadForm() {
         }
     };
 
-    const saveTranscriptionToFirestore = async (transcription) => {
-        if (auth.currentUser) {
-            try {
-                const userRef = doc(db, "users", auth.currentUser.uid);
-                const userDoc = await getDoc(userRef);
+    const saveTranscriptionToFirestore = async () => {
+        if (!auth.currentUser || !transcription) return;
+
+        try {
+            const userRef = doc(db, "users", auth.currentUser.uid);
+            const userDoc = await getDoc(userRef);
     
-                if (!userDoc.exists()) {
-                    console.log('No such user!');
-                    return;
-                }
-    
-                const userData = userDoc.data();
-    
-                await addDoc(collection(db, 'transcriptions'), {
-                    userId: auth.currentUser.uid,
-                    firstName: userData.firstName,
-                    lastName: userData.lastName,
-                    text: transcription,
-                    timestamp: new Date()
-                });
-    
-                console.log('Transcription saved');
-            } catch (error) {
-                console.error('Error saving transcription:', error);
+            if (!userDoc.exists()) {
+                console.log('No such user!');
+                return;
             }
-        } else {
-            console.log('User not logged in');
+    
+            const userData = userDoc.data();
+    
+            await addDoc(collection(db, 'transcriptions'), {
+                userId: auth.currentUser.uid,
+                firstName: userData.firstName,
+                lastName: userData.lastName,
+                text: transcription,
+                timestamp: new Date()
+            });
+    
+            console.log('Transcription saved');
+        } catch (error) {
+            console.error('Error saving transcription:', error);
         }
     };
-    
 
-    const handleSaveTranscription = () => {
-        if (transcription) {
-            saveTranscriptionToFirestore(transcription);
-        }
-    };
+    const handleSaveTranscription = () => saveTranscriptionToFirestore();
+    const handleCloseModal = () => setIsModalOpen(false);
+    const openModal = () => setIsModalOpen(true); 
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
-    
-    
     return (
-        // <div className="file-upload">
-        //     {isModalOpen && (
-        //         <div className="file-upload__modal">
-        //             <div className="file-upload__modal__content">
-        //                 <img className="file-upload__modal__close" src={closeModalIcon} onClick={handleCloseModal} alt="Close modal"></img>
-        //                 <h2 className="file-upload__modal__header">Upload an Audio File</h2>
-        //                 <p className="file-upload__modal__supported">Supported file types: </p>
-        //                 <p className="file-upload__modal__supported--values">mp3, mp4, mpeg, mpga, m4a, wav, and webm</p>
-        //                 <div className="file-upload__modal__buttons">
-        //                     <input type="file" onChange={handleFileChange} className="file-upload__modal__input" />
-        //                     <button className="file-upload__modal__upload" onClick={handleSubmit} disabled={!selectedFile}>Upload</button>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     )}
-        //     {transcription && (
-        //         <div className="file-upload__result">
-        //             <h3 className="file-upload__result--header">Transcription:</h3>
-        //             <p className="file-upload__result--value">{transcription}</p>
-        //             <div className="file-upload__result--buttons">
-        //                 <button className="file-upload__result--save" onClick={handleSaveTranscription}>Save Transcription</button>
-        //                 <button className="file-upload__result--new" onClick={openModal}>Transcribe New File</button>
-        //             </div>
-        //         </div>
-        //     )}
-        // </div>
-        
-        <section className="upload">
-            <div className="upload__header">
-                <img className="upload__header--icon" src={uploadCloudIcon}></img>
-                <h2 className="upload__header--text">Upload your file</h2>
-            </div>
-            <article className="upload__drag">
-                <h3 className="upload__drag--header">Drag & Drop</h3>
-                <p className="upload__drag--supported">Supported format files</p>
-                <p className="upload__drag--filetypes">mp3, mp4, mpeg, mpga, m4a, wav, and webm</p>
-                <img className="upload__drag--icon" src={audioFileIcon}></img>
-                <button className="upload__drag--file-btn">Choose File</button>
-            </article>
-        </section>
-    
+        <>
+            {isModalOpen && (
+                <>
+                    <div className="overlay"></div>
+                    <section className="upload">
+                        <img className="upload__close" src={closeModalIcon} onClick={handleCloseModal} alt="Close Modal" />
+                        <div className="upload__header">
+                            <img className="upload__header--icon" src={uploadCloudIcon} alt="Upload Icon" />
+                            <h2 className="upload__header--text">Upload your file</h2>
+                        </div>
+                        <article className="upload__drag">
+                            <h3 className="upload__drag--header">Drag & Drop</h3>
+                            <p className="upload__drag--supported">Supported format files</p>
+                            <p className="upload__drag--filetypes">mp3, mp4, mpeg, mpga, m4a, wav, and webm</p>
+                            <img className="upload__drag--icon" src={audioFileIcon} alt="Audio File Icon" />
+                            <button className="upload__drag--file-btn" onClick={handleClick}>Choose File</button>
+                            <input 
+                            type="file" 
+                            ref={hiddenFileInput} 
+                            onChange={handleFileChange} 
+                            style={{ display: 'none' }} 
+                        />
+                        </article>
+                    </section>
+                </>
+            )}
+
+            {transcription && (
+                <div className="file-upload__result">
+                    <h3 className="file-upload__result--header">Transcription:</h3>
+                    <p className="file-upload__result--value">{transcription}</p>
+                    <div className="file-upload__result--buttons">
+                        <button className="file-upload__result--save" onClick={handleSaveTranscription}>Save Transcription</button>
+                        <button className="file-upload__result--new" onClick={openModal}>Transcribe New File</button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
