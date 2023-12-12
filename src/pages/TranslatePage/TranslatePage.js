1046c3c948fe3165ee58cc2e968a8d91ac4fa289
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-12-11 22:54:04 -0500

    temporarily renamed files

--- src/pages/TranslatePage/TranslatePage.js
+++ src/pages/TranslatePage/TranslatePage.js
@@ -1,8 +1,8 @@
 import "./TranslatePage.scss";
 import TranslationForm from "../../components/TranslationForm.js/TranslationForm";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { logout } from '../../components/Auth/Auth';
+// import { logout } from '../../components/Auth/Auth';
+import { logout } from "../../components/auth/auth";
 import NavSidebar from "../../components/NavSidebar/NavSidebar";
 
 
